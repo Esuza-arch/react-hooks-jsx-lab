6fa4d753f0c97a65ca97a7ad9754b101bb6a7e66
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-10-31 21:16:39 +0300

    finish lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2><p>I actually made this</p> <img src={image} alt="I made this"/> </div>;
 }
 
 export default About;
